a8bca9129d8b442a1252fb9a24a92c2679c16c47
牛犇 <ben.niu01@hand-china.com>
2019-06-30 00:31:25 +0800

    [UPDATE] 新增zuul组件

---
+++ eureka-zuul-client/src/main/java/com/hand/MyFilter.java
@@ -0,0 +1,54 @@
+package com.hand;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @description: 自定义zuul过滤器
+ * @author: ben.niu01@hand-china.com
+ * @date: 2019/6/20
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            logger.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        logger.info("ok");
+        return null;
+    }
+}
